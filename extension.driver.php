5d2e4e39c432e56d47ef774a83f9af816f14bad8
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-05-23 21:11:04 +0200

    Added database tables to install script

--- extension.driver.php
+++ extension.driver.php
@@ -33,16 +33,53 @@ class extension_email_newsletters extends extension{
 	}
 	
 	public function install(){
-		try{
-			$etm = Symphony::ExtensionManager()->getInstance('email_template_manager');
-			if($etm instanceof Extension){
-				return true;
-			}
-			else{
-				throw new Exception(__('The Email Template Manager is required for this extension to work.'));
-			}
+		$etm = Symphony::ExtensionManager()->getInstance('email_template_manager');
+		if($etm instanceof Extension){
+			Symphony::Database()->query(
+				'CREATE TABLE IF NOT EXISTS
+				`sym_email_newsletters_recipientgroups` (
+					`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+					`name` varchar(255) NOT NULL,
+					`recipients` text,
+					PRIMARY KEY (`id`)
+				)
+				ENGINE=InnoDB 
+				DEFAULT CHARSET=utf8
+				COLLATE=utf8_unicode_ci
+				AUTO_INCREMENT=4'
+			);
+			Symphony::Database()->query(
+				'CREATE TABLE IF NOT EXISTS
+				`sym_email_newsletters_recipientgroups_params` (
+					`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+					`recipientgroup_id` int(10) unsigned NOT NULL,
+					`name` varchar(255) NOT NULL,
+					`value` varchar(255) NOT NULL,
+					PRIMARY KEY (`id`)
+				)
+				ENGINE=InnoDB 
+				DEFAULT CHARSET=utf8
+				COLLATE=utf8_unicode_ci
+				AUTO_INCREMENT=18'
+			);
+			Symphony::Database()->query(
+				'CREATE TABLE IF NOT EXISTS
+				`sym_email_newsletters_senders` (
+					`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+					`name` varchar(255)  NOT NULL,
+					`email` varchar(255)  NOT NULL,
+					`reply-to-name` varchar(255) DEFAULT NULL,
+					`reply-to-email` varchar(255) DEFAULT NULL,
+					PRIMARY KEY (`id`)
+				)
+				ENGINE=InnoDB 
+				DEFAULT CHARSET=utf8
+				COLLATE=utf8_unicode_ci
+				AUTO_INCREMENT=11'
+			);
+			return true;
 		}
-		catch(Exception $e){
+		else{
 			throw new Exception(__('The Email Template Manager is required for this extension to work.'));
 		}
 	}
