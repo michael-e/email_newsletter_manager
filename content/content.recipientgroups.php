14c4e0fdc4c2e6845fffc4884e8ef952f8e452a2
michael-e <info@michael-eichelsdoerfer.de>
2011-06-14 12:46:22 +0200

    Added title to edit page. Unfortunately, the context does not supply an handle-style representation, but only an "underscore" style representation of the group's name.

--- content/content.recipientgroups.php
+++ content/content.recipientgroups.php
@@ -45,6 +45,7 @@ Class contentExtensionemail_newsletter_managerrecipientgroups extends ExtensionP
 
 	function __viewEdit($new = false){
 		$this->setPageType('form');
+		$this->setTitle(sprintf(__("Symphony - Email Recipients - %s", Array(), false), ucfirst($this->_context[1])));
 		$this->addScriptToHead(URL . '/extensions/email_newsletter_manager/assets/email_newsletter_manager.recipientgroups.js', 140);
 		$this->addStylesheetToHead(URL . '/extensions/email_newsletter_manager/assets/email_newsletter_manager.recipientgroups.css', 'screen', 103);
 
