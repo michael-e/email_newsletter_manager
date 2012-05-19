michael-e/email_newsletter_manager

commit 921777baedbd32264f8660487de4d6087ee5d59d

Never start if status is not NULL.
Don't throw an exception, just make the function return. That way the user will simply see the current status after attempting to start the newsletter.

Fixes #37.

lib/class.emailnewsletter.php
@@ -64,19 +64,17 @@ class EmailNewsletter{
 	public function start(){
 		if(!is_object($this->getTemplate())){
 			$this->setStatus('error_template');
-			return;
 		}
 		if(!is_object($this->getSender())){
 			$this->setStatus('error_sender');
-			return;
 		}
 		$rec_groups = $this->getRecipientGroups();
 		if(empty($rec_groups)){
 			$this->setStatus('error_recipients');
-			return;
 		}
-		if($this->getStatus() == 'stopped'){
-			throw new EmailNewsletterException('Can not restart a stopped process. Please start a new process if you need to send again.');
+		// Never start if the newsletter has no virgin state
+		if($this->getStatus() !== NULL){
+			return;
 		}
 		$this->generatePAuth();
 		Symphony::Database()->query("CREATE TABLE IF NOT EXISTS `tbl_tmp_email_newsletters_sent_". $this->getId() . "` (

commit a158f2794678ec382ef23a2e1e10ee0b64228139

Fixes bug introduced by 921777baedbd32264f8660487de4d6087ee5d59d.
The order of errors was wrong. Additionally, making every condition 'self-returning' is much easier to read.

lib/class.emailnewsletter.php
@@ -64,13 +64,16 @@ class EmailNewsletter{
 	public function start(){
 		if(!is_object($this->getTemplate())){
 			$this->setStatus('error_template');
+			return;
 		}
 		if(!is_object($this->getSender())){
 			$this->setStatus('error_sender');
+			return;
 		}
 		$rec_groups = $this->getRecipientGroups();
 		if(empty($rec_groups)){
 			$this->setStatus('error_recipients');
+			return;
 		}
 		// Never start if the newsletter has no virgin state
 		if($this->getStatus() !== NULL){