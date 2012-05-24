71867463cfa075b67a73720a5b8616c1727199a3
michael-e <info@michael-eichelsdoerfer.de>
2012-05-24 14:20:34 +0200

    Call the old, typo'd function name. Fixes #20.
    Symphony aliases the old function name to the new one in class.field.php.

--- lib/class.recipientsourcesection.php
+++ lib/class.recipientsourcesection.php
@@ -193,7 +193,9 @@ Class RecipientSourceSection extends RecipientSource{
 					$where = preg_replace('/`t\d+`.value/', '`e`.creation_date', $where);
 				}
 				else{
-					if(!$fieldPool[$field_id]->buildDSRetrievalSQL($value, $joins, $where, ($filter_type == DS_FILTER_AND ? true : false))){ $this->_force_empty_result = true; return; }
+					// For deprecated reasons, call the old, typo'd function name until the switch to the
+					// properly named buildDSRetrievalSQL function. This is necessary, e.g. for the SBL field.
+					if(!$fieldPool[$field_id]->buildDSRetrivalSQL($value, $joins, $where, ($filter_type == DS_FILTER_AND ? true : false))){ $this->_force_empty_result = true; return; }
 					if(!$group) $group = $fieldPool[$field_id]->requiresSQLGrouping();
 				}
 			}
