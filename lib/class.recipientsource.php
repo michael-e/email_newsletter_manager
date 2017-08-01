michael-e/email_newsletter_manager

commit 20e05647e4b1914da8612f0a3164ab6263ecd716

Change execution order in processDependencies
Fixes #76

lib/class.recipientsource.php
@@ -56,15 +56,6 @@ class recipientsource extends Datasource
 
     public function processDependencies(array $params = array())
     {
-        $datasources = $this->getDependencies();
-
-        if (!is_array($datasources) || empty($datasources)) {
-            return;
-        }
-
-        $datasources = array_map(create_function('$a', "return str_replace('\$ds-', '', \$a);"), $datasources);
-        $datasources = array_map(create_function('$a', "return str_replace('-', '_', \$a);"), $datasources);
-
         $env = array(
             'today' => DateTimeObj::get('Y-m-d'),
             'current-time' => DateTimeObj::get('H:i'),
@@ -77,6 +68,18 @@ class recipientsource extends Datasource
 
         $this->_env['param'] = $env;
         $this->_env['env']['pool'] = $params;
+
+        $this->processParameters($this->_env);
+
+        $datasources = $this->getDependencies();
+
+        if (!is_array($datasources) || empty($datasources)) {
+            return;
+        }
+
+        $datasources = array_map(create_function('$a', "return str_replace('\$ds-', '', \$a);"), $datasources);
+        $datasources = array_map(create_function('$a', "return str_replace('-', '_', \$a);"), $datasources);
+
         $dependencies = array();
 
         foreach ($datasources as $handle) {
@@ -95,7 +98,6 @@ class recipientsource extends Datasource
             $ds->execute($this->_env['env']['pool']);
             unset($ds);
         }
-        $this->processParameters($this->_env);
     }
 
     public function __findDatasourceOrder($dependenciesList)

commit f5b741ed9670c15f9a169347dcf41d72deb9013f

Fix regression introduced by 20e05647e4b1914da8612f0a3164ab6263ecd716

lib/class.recipientsource.php
@@ -69,35 +69,33 @@ class recipientsource extends Datasource
         $this->_env['param'] = $env;
         $this->_env['env']['pool'] = $params;
 
-        $this->processParameters($this->_env);
-
         $datasources = $this->getDependencies();
 
-        if (!is_array($datasources) || empty($datasources)) {
-            return;
+        if (is_array($datasources) && $datasources) {
+            $datasources = array_map(create_function('$a', "return str_replace('\$ds-', '', \$a);"), $datasources);
+            $datasources = array_map(create_function('$a', "return str_replace('-', '_', \$a);"), $datasources);
+
+            $dependencies = array();
+
+            foreach ($datasources as $handle) {
+                $profiler = Symphony::Profiler();
+                $profiler->seed();
+
+                $pool[$handle] =& DatasourceManager::create($handle, null, false);
+                $dependencies[$handle] = $pool[$handle]->getDependencies();
+            }
+
+            $dsOrder = $this->__findDatasourceOrder($dependencies);
+
+            foreach ($dsOrder as $handle) {
+                $ds = $pool[$handle];
+                $ds->processParameters($this->_env);
+                $ds->execute($this->_env['env']['pool']);
+                unset($ds);
+            }
         }
 
-        $datasources = array_map(create_function('$a', "return str_replace('\$ds-', '', \$a);"), $datasources);
-        $datasources = array_map(create_function('$a', "return str_replace('-', '_', \$a);"), $datasources);
-
-        $dependencies = array();
-
-        foreach ($datasources as $handle) {
-            $profiler = Symphony::Profiler();
-            $profiler->seed();
-
-            $pool[$handle] =& DatasourceManager::create($handle, null, false);
-            $dependencies[$handle] = $pool[$handle]->getDependencies();
-        }
-
-        $dsOrder = $this->__findDatasourceOrder($dependencies);
-
-        foreach ($dsOrder as $handle) {
-            $ds = $pool[$handle];
-            $ds->processParameters($this->_env);
-            $ds->execute($this->_env['env']['pool']);
-            unset($ds);
-        }
+        $this->processParameters($this->_env);
     }
 
     public function __findDatasourceOrder($dependenciesList)